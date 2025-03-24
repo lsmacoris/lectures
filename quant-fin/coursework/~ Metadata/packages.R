59170186a2c64b9de0d8d6c4a45f88652ada713d
Lucas S. Macoris <lucasmacoris@gmail.com>
2025-03-23 22:03:13 -0300

    Added L7 quantfin

--- quant-fin/coursework/~ Metadata/packages.R
+++ quant-fin/coursework/~ Metadata/packages.R
@@ -1,5 +1,5 @@
 # Package names
-packages <- c("tidyverse","tidyquant","tidymodels","xts","glue","scales","downloadthis","ggthemes","highcharter")
+packages <- c("tidyverse","tidyquant","tidymodels","xts","glue","scales","downloadthis","ggthemes","highcharter","PerformanceAnalytics","PortfolioAnalytics","DT","DEoptim")
 
 # Install packages not yet installed
 installed_packages <- packages %in% rownames(installed.packages())
