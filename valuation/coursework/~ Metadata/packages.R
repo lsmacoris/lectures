be9818ccac22b070a263d6c8c4cc12f42b603b3e
Lucas S. Macoris <lucasmacoris@gmail.com>
2025-05-04 20:54:51 -0300

    Added L8 quantfin

---
+++ valuation/coursework/~ Metadata/packages.R
@@ -0,0 +1,12 @@
+# Package names
+packages <- c("tidyverse","tidyquant","tidymodels","xts","glue","scales","downloadthis","ggthemes","highcharter","PerformanceAnalytics","PortfolioAnalytics","DT","DEoptim")
+
+# Install packages not yet installed
+installed_packages <- packages %in% rownames(installed.packages())
+
+if (any(installed_packages == FALSE)) {
+  install.packages(packages[!installed_packages])
+}
+
+# Load all packages
+invisible(lapply(packages, library, character.only = TRUE))
